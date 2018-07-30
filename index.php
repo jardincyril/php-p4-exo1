8d00acdf1030baf67d2db1a7ac26e0f0a5c53378
cyril jardin <cyilroc60@hotmail.fr>
2018-07-30 09:32:02 +0200

    fonction

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+<head>
+  <meta charset="utf-8">
+  <title>exo1</title>
+</head>
+<body>
+  <p>
+    <?php
+    function returnTrue() {
+      return true;
+    }
+    echo returnTrue();
+    ?>
+  </p>
+</body>
+</html>
